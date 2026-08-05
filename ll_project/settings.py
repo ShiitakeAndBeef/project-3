cda3e379d6fa07630eac97fcac4a6942222f6ea6
ShiitakeAndBeef <ilovebunnies4578@gmail.com>
2026-08-04 21:13:05 -0600

    Configure project for PythonAnywhere deployment

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -21,15 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v(cg3&a@__)7&n_$9+o&o$_xp)kbv7eq=61_$96ayo)&m3sb6+'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-fallback-for-dev-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['172.31.28.174', 'localhost', '127.0.0.1']
-#ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'YOURUSERNAME.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'tmonarco.pythonanywhere.com']
 
-#CSRF_TRUSTED_ORIGINS = ['https://YOURUSERNAME.pythonanywhere.com']
+CSRF_TRUSTED_ORIGINS = ['https://tmonarco.pythonanywhere.com']
 
 
 # Application definition
@@ -133,4 +132,5 @@ LOGIN_URL = 'accounts:login'
 LOGOUT_REDIRECT_URL = 'learning_logs:index'
 
 # Static files, needed for deployment where runserver no longer serves them.
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
