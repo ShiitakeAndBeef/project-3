1ad22c57837aacf78d10772b531567225da604c6
ShiitakeAndBeef <ilovebunnies4578@gmail.com>
2026-08-04 21:31:21 -0600

    Bug Fixing 4, making more secure

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-fallback-for-dev-only')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-backup-for-dev-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
