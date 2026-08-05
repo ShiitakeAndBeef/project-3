4f928c193b3c9f633755a94dab8f3d2f033d4452
ShiitakeAndBeef <ilovebunnies4578@gmail.com>
2026-08-04 20:37:05 -0600

    Bug fixing 2, fixed the Topic Page

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-v(cg3&a@__)7&n_$9+o&o$_xp)kbv7eq=61_$96ayo)&m3sb6+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'YOURUSERNAME.pythonanywhere.com']
+
+#CSRF_TRUSTED_ORIGINS = ['https://YOURUSERNAME.pythonanywhere.com']
 
 
 # Application definition
